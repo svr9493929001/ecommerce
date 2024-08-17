56ef389ce9f169ab0880cda2844d2e8a3a10f367
sri.v.polireddy <sri.v.polireddy@accenture.com>
2024-08-17 16:57:17 +0530

    commit 8

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -24,7 +24,7 @@ const FeaturedProducts = () => {
                     >
             <img className="d-block w-100" src="https://images.pexels.com/photos/20903471/pexels-photo-20903471/free-photo-of-all-day-matte-foundation-by-nykka-commercial-photo.jpeg?auto=compress&cs=tinysrgb&w=600" alt="First slide" style={{ maxHeight: "500px" }} />
            
-            <Carousel.Caption style={{ color: "black" }}>
+            <Carousel.Caption>
               <h1>nykaa Products </h1>
               <h6>Starting at <strong style={{ textDecoration: 'line-through', fontSize: "Bold" }}>$999</strong> $899</h6>
             </Carousel.Caption>
@@ -46,7 +46,7 @@ const FeaturedProducts = () => {
                         }}
                     >
             <img className="d-block w-100" src="https://images.pexels.com/photos/8015774/pexels-photo-8015774.jpeg?auto=compress&cs=tinysrgb&w=600" alt='second img' style={{ maxHeight: "500px" }} />
-            <Carousel.Caption>
+            <Carousel.Caption style={{ color: "black" }}>
               <h1>bath products </h1>
               <h6>Starting at <strong style={{ textDecoration: 'line-through', fontSize: "Bold" }}>$599</strong> $499</h6>
             </Carousel.Caption>
